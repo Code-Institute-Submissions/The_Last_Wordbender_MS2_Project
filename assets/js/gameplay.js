1a1971d9180ccfd25eded9277766e67d618bb6f7
oludare89 <azizoyekan@hotmail.com>
2021-01-20 23:04:25 +0000

    Added variables with the clues for the different game difficulties in gameplay.js

--- assets/js/gameplay.js
+++ assets/js/gameplay.js
@@ -1,9 +1,13 @@
-var easyGame = ["calm", "lantern", "hammer", "divine", "emotion", "rotation", "karma", "revenge", "shipwreck", "poetry"]
+var easyGameWords = ["calm", "lantern", "hammer", "divine", "emotion", "rotation", "karma", "revenge", "shipwreck", "poetry"]
+var easyGameClues = ["peace", "guiding light", "a workman's tool", "holy", "feelings", "circular", "what comes around goes around", "payback", "the Titanic", "... in motion"]
 
-var mediumGame = ["rejoice", "elevate", "invulnerable", "liberty", "scarce", "zealot", "heretic", "subordinate", "citadel", "paramount"]
+var mediumGameWords = ["rejoice", "elevate", "invulnerable", "liberty", "scarce", "zealot", "heretic", "subordinate", "citadel", "paramount"]
+var mediumGameClues = ["celebrate", "higher", "immune to damage", "freedom", "hard to find", "a true fanatic", "a blasphemous person", "member of the rank and file", "fortress", "important"]
 
-var hardGame = ["tyrannical", "meticulous", "miniscule", "flagrant", "juxtaposition", "paradox", "philanthropic", "parabolic", "emphatic", "prehistoric" ]
+var hardGameWords = ["tyrannical", "meticulous", "miniscule", "flagrant", "juxtaposition", "paradox", "philanthropic", "impervious", "emphatic", "prehistoric" ]
+var hardGameClues = ["oppressive", "in great detail", "tiny", "brazen wrongdoing", "direct comparison", "a contradiction", "pursuits of charity", "no entry", "forceful", "time of the dinosaurs"]
 
 function anagram() {
-
+    console.log(easyGame.splice(0));
+    
 }
