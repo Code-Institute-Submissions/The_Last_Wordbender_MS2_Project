7fbd8caa6b63a0635d20c2893410d943ebd115f8
oludare89 <azizoyekan@hotmail.com>
2021-01-20 14:25:14 +0000

    added words for each game difficulty in variables in gameplay.js

---
+++ assets/js/gameplay.js
@@ -0,0 +1,9 @@
+var easyGame = ["calm", "lantern", "hammer", "divine", "emotion", "rotation", "karma", "revenge", "shipwreck", "poetry"]
+
+var mediumGame = ["rejoice", "elevate", "invulnerable", "liberty", "scarce", "zealot", "heretic", "subordinate", "citadel", "paramount"]
+
+var hardGame = ["tyrannical", "meticulous", "miniscule", "flagrant", "juxtaposition", "paradox", "philanthropic", "parabolic", "emphatic", "prehistoric" ]
+
+function anagram() {
+
+}
