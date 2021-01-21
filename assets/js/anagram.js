01a674c2d2697e6771f12c27160082a3e5867a2c
oludare89 <azizoyekan@hotmail.com>
2021-01-21 20:53:57 +0000

    added comments to index.js and anagram.js files and put default for sound to be off in the anagram.js file

--- assets/js/anagram.js
+++ assets/js/anagram.js
@@ -1,14 +1,28 @@
+// default setting of sound being off
+sessionStorage.setItem("sound", "off")
+
 $(document).ready(function() {
+
+    // this is to activate the how to play modal being shown
     $("#how-to-play").click(function() {
         $(".htp-modal").addClass("bg-active-modal");
     });
-    $(".close-btn").click(function() {
-        $(".htp-modal").removeClass("bg-active-modal");
-    });
+
+    // this is to activate the high scores modal being shown
     $("#high-scores").click(function() {
         $(".high-score-modal").addClass("bg-active-modal");
     });
+
+    // this is to close any modals in the html file
+    $(".close-btn").click(function() {
+        $(".htp-modal").removeClass("bg-active-modal");
+        $(".high-score-modal").removeClass("bg-active-modal");
+    });
+
+    
+
     $(".close-btn").click(function() {
         $(".high-score-modal").removeClass("bg-active-modal");
     });
+
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+
+    // this is to activate the inactive modal for the game modes not yet developed
     $(".inactive").click(function() {
         $(".base-modal").addClass("bg-active-modal");
     });
+
+    // this is to close the modal on the index page
     $(".close-btn").click(function() {
         $(".base-modal").removeClass("bg-active-modal");
     });
