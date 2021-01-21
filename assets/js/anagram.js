8dde09e926fd0604f3dd44089073a28356b2c054
oludare89 <azizoyekan@hotmail.com>
2021-01-21 20:59:59 +0000

    Added javascript to set game difficulty when selectingeasy, medium or hard on the anagram home page

--- assets/js/anagram.js
+++ assets/js/anagram.js
@@ -19,10 +19,20 @@ $(document).ready(function() {
         $(".high-score-modal").removeClass("bg-active-modal");
     });
 
-    
+    //below codes is for setting the game difficulty
+    $("#btn-easy").click(function() {
+        sessionStorage.setItem("gamemode", "easy");
+        sessionStorage.setItem("pointsPerAnswer","50");
+    });
 
-    $(".close-btn").click(function() {
-        $(".high-score-modal").removeClass("bg-active-modal");
+    $("#btn-medium").click(function() {
+        sessionStorage.setItem("gamemode", "medium");
+        sessionStorage.setItem("pointsPerAnswer","100");
+    });
+
+    $("#btn-easy").click(function() {
+        sessionStorage.setItem("gamemode", "hard");
+        sessionStorage.setItem("pointsPerAnswer","200");
     });
 
 });
