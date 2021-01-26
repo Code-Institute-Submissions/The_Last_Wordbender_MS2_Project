fd761100a04d7ff3f98cf752399fc11b9c48f03c
oludare89 <azizoyekan@hotmail.com>
2021-01-26 23:34:20 +0000

    Amended inactive modal styling

--- assets/js/anagram.js
+++ assets/js/anagram.js
@@ -7,18 +7,18 @@ $(document).ready(function() {
 
     // this is to activate the how to play modal being shown
     $("#how-to-play").click(function() {
-        $(".htp-modal").addClass("bg-active-modal");
+        $(".htp-modal").addClass("reveal");
     });
 
     // this is to activate the high scores modal being shown
     $("#high-scores").click(function() {
-        $(".high-score-modal").addClass("bg-active-modal");
+        $(".high-score-modal").addClass("reveal");
     });
 
     // this is to close any modals in the html file
     $(".close-btn").click(function() {
-        $(".htp-modal").removeClass("bg-active-modal");
-        $(".high-score-modal").removeClass("bg-active-modal");
+        $(".htp-modal").removeClass("reveal");
+        $(".high-score-modal").removeClass("reveal");
     });
 
     //This is for setting the game difficulty
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     // this is to activate the inactive modal for the game modes not yet developed
-    $(".inactive").click(function() {
-        $(".inactive-modal").addClass("bg-active-modal")
+    $(".inactive-landing-btn").click(function() {
+        $(".inactive-modal").addClass("reveal")
     });
 
     // this is to close the modal on the index page
     $(".close-btn").click(function() {
-        $(".inactive-modal").removeClass("bg-active-modal")
+        $(".inactive-modal").removeClass("reveal")
     });
 });
